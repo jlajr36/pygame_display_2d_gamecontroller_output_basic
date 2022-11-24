a126d834be13a54c75830d1fe57cca722d965e6c
John Amos <jlajr36@gmail.com>
2022-11-24 11:42:27 -0600

    Draw static cross lines.

--- app.py
+++ app.py
@@ -6,11 +6,20 @@ pygame.init()
 #colors
 background_color = (255, 255, 255)
 text_color = (0, 0, 0)
+line_color = (255, 0, 0)
 
 #window setup
-size = (640, 480)
+width = 640
+height = 480
+size = (width, height)
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption('2D Controller Display')
+
+#lines start pos
+top = 0
+left_side = 0
+center_width = width/2
+center_height = height/2
 
 #setup position font
 font_size = 18
@@ -29,13 +38,18 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    #draw screen and elements
+    #draw screen background
     screen.fill(background_color)
 
+    #display text position
     text_1 = text_font.render("test1", True, text_color)
     text_2 = text_font.render("test2", True, text_color)
     screen.blit(text_1, text_pos_1)
     screen.blit(text_2, text_pos_2)
+
+    #draw lines
+    pygame.draw.line(screen, line_color, (center_width, top), (center_width, height))
+    pygame.draw.line(screen, line_color, (left_side, center_height), (width, center_height))
 
     #update display
     pygame.display.flip()
