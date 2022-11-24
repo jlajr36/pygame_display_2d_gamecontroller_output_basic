fa94377e9f2a12b8f3ca4dacbe092f1a46e3352b
John Amos <jlajr36@gmail.com>
2022-11-24 09:27:12 -0600

    Add static text.

--- app.py
+++ app.py
@@ -5,12 +5,20 @@ pygame.init()
 
 #colors
 background_color = (255, 255, 255)
+text_color = (0, 0, 0)
 
 #window setup
 size = (640, 480)
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption('2D Controller Display')
 
+#setup position font
+font_size = 18
+bold = False
+italicized = False
+text_pos_1 = [500, 0]
+text_pos_2 = [500, 20]
+text_font = pygame.font.SysFont('Calibri', font_size, bold, italicized)
 
 #main game loop
 running = True
@@ -23,6 +31,11 @@ while running:
 
     #draw screen and elements
     screen.fill(background_color)
+
+    text_1 = text_font.render("test1", True, text_color)
+    text_2 = text_font.render("test2", True, text_color)
+    screen.blit(text_1, text_pos_1)
+    screen.blit(text_2, text_pos_2)
 
     #update display
     pygame.display.flip()
